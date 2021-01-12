cf6e0dd974b336dc15c836a9026be630afb76dde
zZ-O-O-M <stepanyukun@mail.ru>
2021-01-12 14:59:40 +0700

    change show carts data

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -19,37 +19,59 @@ class CartController extends Controller {
 
       // If we get only one note
       if ($get_params['id']) {
-         $id                            = $get_params['id'];
-         $this->pageTpl                 = "v_oneCart.php";
-         $oneCart                       = $this->model->getOneCart($id);
-         $this->pageData['note']        = $oneCart;
-         $this->pageData['fieldTitles'] = TableMethods::getFieldTitles($oneCart);
-         $this->pageData['title']       = "Вагон #" . $id;
+         $id                      = $get_params['id'];
+         $this->pageTpl           = "v_oneCart.php";
+         $oneCart                 = $this->model->getOneCart($id);
+         $this->pageData['note']  = $oneCart;
+         $this->pageData['title'] = "Вагон #" . $id;
 
          $this->pageData['formUri'] = '/cart/savingOneCrash';
 
-         $this->pageData['fields']     = [
+         $this->pageData['fields'] = [
             ['number', 'Номер', true],
             ['year', 'Год', true],
             ['contract', 'Контракт', true],
          ];
+
          $this->pageData['allPlaces']  = $this->model->getAllPlacesForCart();
          $this->pageData['allTeams']   = $this->model->getAllTeamsForCart();
          $this->pageData['allTeams'][] = $oneCart['team'];
 
-         // if we get all notes
-      } else {
-         $this->pageTpl = "v_allCarts.php";
-
-         $allCarts                      = $this->model->getAllCarts();
-         $this->pageData['notes']       = $allCarts;
-         $this->pageData['fieldTitles'] = TableMethods::getFieldTitles($allCarts[0]);
+         /************ DATA FOR ALL CART'S CRASHES ***********/
+         $allCrashes                    = $oneCart['crashes'];
+         $this->pageData['notes']       = $allCrashes;
+         $this->pageData['fieldTitles'] = TableMethods::getFieldTitles($allCrashes[0]);
 
          // delete id from table
          unset($this->pageData['fieldTitles'][0]);
 
          // set russian titles to show table
-         $this->pageData['tableTitlesName'] = ['Номер', 'Год', 'Договор', 'Бригада', 'Расположение'];
+         $this->pageData['tableTitlesName'] = ['Описание', 'Тип', 'Статус'];
+
+         // if we get all notes
+      } else {
+         $this->pageTpl = "v_allCarts.php";
+
+         $allCarts                = $this->model->getAllCarts();
+         $this->pageData['notes'] = $allCarts;
+//         $this->pageData['fieldTitles'] = TableMethods::getFieldTitles($allCarts[0]);
+         $this->pageData['fieldTitles'] = [
+            'number',
+            'year',
+            'contract',
+            'team',
+            'place'
+         ];
+         // delete id from table
+//         unset($this->pageData['fieldTitles'][0]);
+
+         // set russian titles to show table
+         $this->pageData['tableTitlesName'] = [
+            'Номер',
+            'Год',
+            'Договор',
+            'Бригада',
+            'Расположение'];
 
          $this->pageData['title'] = "Список вагонов";
       }
--- app/views/v_oneCart.php
+++ app/views/v_oneCart.php
@@ -27,14 +27,58 @@
     </select>
   </div>
 <?php else: ?>
+
   <div class="message-in-form">
     <p>Нет свободных бригад</p>
   </div>
+
 <?php endif; ?>
 
   <button type="submit" class="btn btn-primary">Изменить</button>
   </form>
   </div>
+  </div>
+  </div>
+
+  <h1 class="cart-crasher-title">Неисправности вагона</h1>
+  <div class="cart-table-for-crashes">
+    <div class="wide-container">
+      <main class="content">
+        <div class="info">
+          <table class="table">
+            <thead>
+            <tr>
+               <?php if ($pageData['tableTitlesName']): ?>
+                  <? foreach ($pageData['tableTitlesName'] as $title): ?>
+                   <th scope="col"><?= $title ?></th>
+                  <? endforeach; ?>
+               <?php endif; ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if ($pageData['notes']): ?>
+               <? foreach ($pageData['notes'] as $note): ?>
+                  <? foreach ($pageData['fieldTitles'] as $title): ?>
+                     <?php if ($title == 'description'): ?>
+                    <td><a href="/crash?id=<?= (string)$note['id'] ?>"><?= $note[$title] ?></a></td>
+                     <?php else: ?>
+                        <?php if (isset($note[$title]['name'])): ?>
+                      <td><?= $note[$title]['name'] ?></td>
+                        <?php elseif (isset($note[$title]['number'])): ?>
+                      <td><?= $note[$title]['number'] ?></td>
+                        <? else: ?>
+                      <td><?= $note[$title] ?></td>
+                        <?php endif; ?>
+                     <?php endif; ?>
+                  <? endforeach; ?>
+                </tr>
+               <? endforeach; ?>
+            <?php endif; ?>
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
   </div>
 
 <?php include VIEW_PATH . "baseTpl/footerTpl.php"; ?>
